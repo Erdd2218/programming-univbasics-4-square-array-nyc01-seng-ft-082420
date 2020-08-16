1b0a34eedb42b7cc77a9b835c03459a2485592b4
Andre Pierre <andre.laurence94@gmail.com>
2020-08-16 05:37:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,21 @@
 def square_array(array)
+<<<<<<< HEAD
   i = 0
   sqrArr = []
   
   while i < array.length do 
   sqrArr << array[i] ** 2
     i += 1
+=======
+  your code here
+  i = 0
+  sqrArr = []
+  
+  while (i < array.length)
+  
+  sqrArr << (array[i] * 2)
+    i += 1 
+>>>>>>> 9e6ddba568e98e8a4ac8913883efff3abf7704ec
     
   end
   sqrArr
